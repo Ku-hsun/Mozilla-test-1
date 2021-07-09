3a327bd5c726140faeac20ccebaba242bd7f427d
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-07-10 03:10:27 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,28 +8,3 @@ myImage.onclick = function() {
         myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-}
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-myButton.onclick = function() {
-    setUserName();
-}
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if(!myName || myName === null) {
-        setUserName();
-    } else {
-        localStorage.setItem('name', myName);
-        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-    }
-}
